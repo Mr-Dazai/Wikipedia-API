6e16ccad9acfc2b9f45c55ef6404a7ef9b753674
Sebin Joe Paul C.M <sebin.joepaul@gmail.com>
2023-06-23 17:59:43 -0400

    app.js RenderResult update

--- app.js
+++ app.js
@@ -34,5 +34,18 @@ const fetchPages = async (searchValue) => {
 };
 
 const renderResults = (list) => {
-  console.log(list);
+  const cardsList = list
+    .map((item) => {
+      const { title, snippet, pageid } = item;
+      return `<a href=http://en.wikipedia.org/?curid=${pageid} target="_blank">
+    <h4>${title}</h4>
+    <p>
+      ${snippet}
+    </p>
+  </a>`;
+    })
+    .join("");
+  result.innerHTML = `<div class="articles">
+          ${cardsList}
+        </div>`;
 };
